6d42456b32dcd100a15a19d3640f0f93bf794932
subin9898 <yewoo28@gamil.com>
2023-07-10 18:10:07 +0900

    rede

---
+++ REDESIGN/js/kooksoondang.js
@@ -0,0 +1,209 @@
+$(document).ready(function () {
+
+
+  //---header고정
+
+  $(window).scroll(function () {
+    let winTop = $(window).scrollTop()
+    if (winTop >= 350) {
+      $("header").addClass("small")
+    } else {
+      $("header").removeClass("small")
+    }
+  })
+
+  //--main_product
+
+  $(window).scroll(function () {
+    let main_product = $(window).scrollTop()
+    if (main_product >= 100) {
+      $(".inner_product>ul").addClass("on")
+    }
+  })
+
+  //----top버튼
+
+  $(".top_button").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 500)
+  })
+
+  //---------------------------------------------------------
+  $(".promo_list>li").click(function (e) {
+    e.preventDefault()
+    $(".promo_list>li").removeClass("on")
+    $(this).addClass("on")
+  })
+  //--------------------------------------------------------
+
+
+
+
+
+
+
+
+  //---- promotion 슬라이드
+
+  let station1 = new Swiper(".vid_station", {
+    loop: true,
+    direction: "horizontal",
+    slidesPerView: 3,  //슬라이드 개수의 반만큼 최대 적용 가능
+    spaceBetween: 15,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".csNextBtn",
+      prevEl: ".csPrevBtn",
+    },
+
+
+    // breakpoints: {
+    //   320: {
+    //     slidesPerView: 1,
+    //     spaceBetween: 0
+    //   },
+    //   768: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 10
+    //   },
+    //   1024: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 20
+    //   }
+    // }
+  })
+
+
+
+  let station2 = new Swiper(".img_station", {
+    loop: true,
+    direction: "horizontal",
+    slidesPerView: 3,  //슬라이드 개수의 반만큼 최대 적용 가능
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".csNextBtn",
+      prevEl: ".csPrevBtn",
+    },
+  })
+  //----------------------promo popup1
+  $(".mak").click(function (e) {
+    e.preventDefault()
+    $(".vid_popup1").addClass("on")
+  })
+  $(".btnMenu").click(function (e) {
+    e.preventDefault()
+    $(".vid_popup1").removeClass("on")
+  })
+  //-------------------------------------
+  $(".beak").click(function (e) {
+    e.preventDefault()
+    $(".vid_popup2").addClass("on")
+  })
+  $(".btnMenu").click(function (e) {
+    e.preventDefault()
+    $(".vid_popup2").removeClass("on")
+  })
+
+
+
+
+
+
+
+
+  //-----------------woori_popup
+
+  $(".woori_1-2>ul>li.rawrice").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup1").addClass("on")
+  })
+
+  $(".btnMenu").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup1").removeClass("on")
+  })
+  //-----------------------------------------------------------
+  $(".woori_1-2>ul>li.jurice").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup2").addClass("on")
+  })
+
+  $(".btnMenu").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup2").removeClass("on")
+  })
+  //---------------------------------------------------------------
+  $(".woori_1-2>ul>li.promise").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup4").addClass("on")
+  })
+
+  $(".btnMenu").click(function (e) {
+    e.preventDefault()
+    $(".woori_popup4").removeClass("on")
+  })
+
+
+
+
+  //-------------- woori_content2
+
+  $(window).scroll(function () {
+    let winTop = $(window).scrollTop()
+    if (winTop >= 1300) {
+      $(".woori_2").addClass("on")
+    }
+  })
+  $(window).scroll(function () {
+    let winTop = $(window).scrollTop()
+    if (winTop >= 1350) {
+      $(".woori_2-2").addClass("on")
+    }
+  })
+  $(window).scroll(function () {
+    let winTop = $(window).scrollTop()
+    if (winTop >= 1450) {
+      $(".woori_2-3").addClass("on")
+    }
+  })
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,233 @@
+$(document).ready(function () {
+  // let pathLength = $(".letter").get(0).getTotalLength()
+  //  get(0)-> 제이쿼리 문서객체를 자바스크립트 문서객체로 변환하여 리턴함
+  // $(".letter").css("stroke-dasharray", pathLength)
+  // $(".letter").css("stroke-dashoffset", pathLength)
+
+  $("#introtxt path").each(function () {
+    let path = $(this)
+    let idx = path.index()
+    let secondTerm = 0.1
+    let delay = idx * secondTerm
+
+
+    let pathLength = $(this).get(0).getTotalLength()
+    $(this).css("stroke-dasharray", pathLength)
+    $(this).css("stroke-dashoffset", pathLength)
+
+    setTimeout(function () {
+      path.css("transition", `stroke-dashoffset 1s ease ${delay}s, fill 1s ease ${delay + 1}s`)
+    }, 300)
+  })
+
+  setTimeout(function () {
+    $("#introtxt").addClass("on")
+
+  }, 500)
+  //-> 메인페이지 svg설정
+
+  //------------------------------------------------------------------
+  $(".all>div").on("wheel DOMMouseScroll", function (event) {
+    //on -> 이벤트를 여러개 묶어 사용할 수 있다.
+    // console.log(event)
+    let E = event.originalEvent
+    let delta = 0;
+    if (E.detail) {
+      delta = E.detail * -40
+    } else {
+      delta = E.wheelDelta
+    }
+    //--->  브라우저 호환성을 맞취주는 설정
+
+    if ($(this).attr("class") == "redesign") {
+      return
+    }
+
+    if ($(this).attr("end") == "redesign") {
+      return
+    }
+
+    if (delta < 0) {
+      //마우스 휠을 내렸을 때
+      if ($(this).next().length != 0) {
+        let posTop = $(this).next().offset().top
+        // console.log(posTop)
+        $("html,body").stop().animate({ scrollTop: posTop }, 1000)
+      }
+    } else {
+      //마우스 휠을 올렸을 때
+      if ($(this).prev().length != 0) {
+        let posTop = $(this).prev().offset().top
+        // console.log(posTop)
+        $("html,body").stop().animate({ scrollTop: posTop }, 1000)
+      }
+    }
+    return false
+  })
+  // -> 스크롤 한뼘이동
+
+
+
+
+
+  // -----------------------------------------------------------------
+
+  let menuState = false;
+
+
+  $(".Menubtn").click(function () {
+    if (menuState == false) {
+      $(".Menubtn").addClass("close")
+      $(".back").addClass("show")
+      $("html").css("overflow", "hidden")
+      menuState = true
+    } else {
+      $(".Menubtn").removeClass("close")
+      $(".back").removeClass("show")
+      $("html").css("overflow", "auto")
+
+      menuState = false
+    }
+  })
+
+  //->메인페이지 메뉴창
+  //-----------------------------------------
+
+  // $(".gnb li").click(function(){
+  //   $(this)
+  // })
+
+
+  //-----------------------------------------------------------------------
+
+  let pathLength = $("rect").get(0).getTotalLength()
+  // console.log(pathLength)
+  $(".rect rect").css("stroke-dasharray", pathLength)
+  $(".rect rect").css("stroke-dashoffset", pathLength)
+
+  //LANGUAGES창에 닿으면 drawSquare가 실행되도록
+
+  $(window).scroll(function () {
+    //스크롤바가 움직일 때 마다 실행되는 소스코드
+    let a = $(window).scrollTop()//스크롤바가 위에서 얼만큼 내려와있는지 계산
+    let b = $(".LANGUAGES").offset().top
+    let c = $(window).height() * 0.5
+
+    if (a + c >= b) {
+      if ($(".LANGUAGES").hasClass("on") == false) {
+
+        $(".LANGUAGES").addClass("on")
+        drawSquare()
+      }
+    }
+  })
+
+
+  // drawSquare()
+  // $(".rect1 rect").css("stroke-dashoffset", pathLength - (pathLength * 0.85))
+  // $(".rect2 rect").css("stroke-dashoffset", pathLength - (pathLength * 0.8))
+  // $(".rect3 rect").css("stroke-dashoffset", pathLength - (pathLength * 0.7))
+  // $(".rect4 rect").css("stroke-dashoffset", pathLength - (pathLength * 0.75))
+
+
+  function drawSquare() {
+    $(".skillper").each(function () {
+      let div = $(this)
+      let rect = $(this).find("rect")
+      let percent = Number($(this).find(".per").text())
+      let count = 0
+      let timer = setInterval(function () {
+        count++
+        div.find(".per").text(count + "%")
+        rect.css("stroke-dashoffset", pathLength - (pathLength * (count / 100)))
+        if (count >= percent) {
+          clearInterval(timer)
+        }
+      }, 10)
+    })
+  }
+
+
+  //-> svg퍼센트
+
+
+  // --------------------------------------------------------------------------
+
+  let count = 0;
+  $(".fa-angle-right").click(function () {
+    count++
+    if (count > 3) { count = 0 }
+    $(".train>li").css("transform", "translateX(" + -1290 * count + "px)")
+  })
+
+  $(".fa-angle-left").click(function () {
+    count--
+    if (count < 0) { count = 3 }
+    $(".train>li").css("transform", "translateX(" + -1290 * count + "px)")
+  })
+
+  //->배너 슬라이드
+
+  // ----------------------------------------------------------------------------
+
+
+  $(".train>li").click(function (e) {
+    e.preventDefault()
+    let idx = $(this).index()
+    // alert(idx)
+    $(".banner > div").removeClass("show")
+    $(".banner > div").eq(idx).addClass("show")
+
+  })
+
+  $(".popupclosebtn").click(function (e) {
+    e.preventDefault()
+    // $(".bannerPopup").removeClass("show")
+    $(".banner > div").removeClass("show")
+    // $(".banner > div").eq(idx).removeClass("show")
+
+  })
+
+  //->배너 팝업
+
+  // ---------------------------------------------------------------------------------
+  let count2 = 0;
+  $(".station2").on("wheel DOMMouseScroll", function (event) {
+    //on -> 이벤트를 여러개 묶어 사용할 수 있다.
+    // console.log(event)
+    let E = event.originalEvent
+    let delta = 0;
+    if (E.detail) {
+      delta = E.detail * -40
+    } else {
+      delta = E.wheelDelta
+    }
+    //--->  브라우저 호환성을 맞취주는 설정
+
+    if (delta < 0) {
+      //마우스 휠을 내렸을 때
+      count2++
+      console.log(count2)
+      if (count2 > 7) { count2 = 0 }
+      $(".train2").css("transform", "translateX(" + (-1020 * count2) + "px)")
+    } else {
+      //마우스 휠을 올렸을 때
+      count2--
+      console.log(count2)
+      if (count2 < 0) { count2 = 7 }
+      $(".train2").css("transform", "translateX(" + (-1020 * count2) + "px)")
+    }
+    return false
+    //이벤트 버블링을 막아줌
+  })
+
+  //-> POPUP 스크롤 슬라이드
+
+  //-------------------------------------------------------------------------------
+
+
+
+
+
+
+})
